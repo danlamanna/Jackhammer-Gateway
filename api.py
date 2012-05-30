e695ce2a9c926fdae990578a8b6ca868058cd291
Christopher Kotifla <kotfic@gmail.com>
2012-05-30 19:42:25 -0400

    remove working directory dependency

--- api.py
+++ api.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 app = Flask(__name__)
 
 import json
+import os
 
 """
 Create a new project  - POST /project/create
@@ -27,7 +28,7 @@ def create_project():
         else:
             project_manifest["projects"][project_slug] = project_data
             
-            json.dump(project_manifest, open("project_manifest.json", "w+"), indent=4)
+            json.dump(project_manifest, open(get_base_dir("project_manifest.json"), "w+"), indent=4)
 
             ret_data = { project_slug: project_manifest["projects"][project_slug] }
 
@@ -39,7 +40,7 @@ def create_project():
 @app.route("/projects/read")
 def read_projects():
     try:
-        project_manifest = json.load(open("project_manifest.json"))
+        project_manifest = json.load(open(get_base_dir("project_manifest.json")))
         
         return success_json(project_manifest)
     except IOError:
@@ -48,7 +49,7 @@ def read_projects():
 @app.route("/project/read/<project_slug>")
 def read_project(project_slug):
     try:
-        project_manifest = json.load(open("project_manifest.json"))
+        project_manifest = json.load(open(get_base_dir("project_manifest.json")))
 
         return success_json(project_manifest["projects"][project_slug])            
     except IOError:
@@ -59,18 +60,23 @@ def read_project(project_slug):
 # delete
 @app.route("/project/delete/<project_slug>")
 def delete_project(project_slug):
-    project_manifest = json.load(open("project_manifest.json"))
+    project_manifest = json.load(open(get_base_dir("project_manifest.json")))
 
     if project_slug not in project_manifest["projects"]:
         return error_json("", "Project doesn't exist in manifest.")
     else:
         del(project_manifest["projects"][project_slug])
         
-        json.dump(project_manifest, open("project_manifest.json", "w+"), indent=4)
+        json.dump(project_manifest, open(get_base_dir("project_manifest.json"), "w+"), indent=4)
 
         return success_json("", "Project deleted.")
 
     return error_json("", "Something went wrong!")
+
+
+def get_base_dir(file_name):
+    return os.path.abspath(os.path.dirname(__file__)) + "/" + file_name
+
 
 def error_json(data, message=""):
     return jsonify(success=0,
